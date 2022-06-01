2fe5444bb3833b628854c62a9acc60d0960c5ba5
kush-munot <munot.kgm@gmail.com>
2022-06-01 12:45:47 +0530

    Added the final Hero Section

--- rcoem-overflow/src/Components/HeroSec.jsx
+++ rcoem-overflow/src/Components/HeroSec.jsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import search from "../assets/search.png"
+import Image from 'react-bootstrap/Image'
+import Button from '@mui/material/Button';
+import ExploreIcon from '@mui/icons-material/Explore';
+import "../index.css";
 
 export default function Boxes() {
   return (
@@ -32,13 +37,52 @@ export default function Boxes() {
         <Box
             sx={{
             width: 445,
-            height: 270,
+            height: 310,
             backgroundColor: '#E26639',
             position:'relative',
             bottom: 330,
-            borderRadius:'30px',
+            borderRadius:'30px'
             }}
         >
+        <div className='imageDiv'>
+            <Image className='search' src={search} />            
+        </div>
+        <Grid container justifyContent="center">
+            <Typography variant="h6" sx={{
+                width: 400,
+                paddingTop: 5,
+                color: 'white'
+            }}>
+                Find the best Answers to your Teachnical Questions, Help Others answer their's
+            </Typography>
+            <Typography variant="body1" component='a' href='#' sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                color: 'white',                
+                textDecoration: 'none',
+                cursor: 'pointer',
+                paddingTop: 2,
+                "&:hover": {
+                    color: "#41D450",
+                    opacity: 10,
+                }
+              }}>
+                Search Question
+            </Typography>            
+        </Grid>
+        <Grid container justifyContent="center" sx={{
+            paddingTop: 2
+        }}>
+            <Button className='exploreButton' variant='contained' startIcon = {<ExploreIcon/>} sx={{
+                backgroundColor:'#20D867',
+                "&:hover": {
+                    backgroundColor: "#41D450",
+                    opacity: 10,
+                }
+
+            }}>Explore</Button>
+        </Grid>       
+
         </Box>
     </Grid>
     
--- rcoem-overflow/src/Components/Navbar.jsx
+++ rcoem-overflow/src/Components/Navbar.jsx
@@ -15,7 +15,6 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import SearchIcon from '@mui/icons-material/Search';
-import logo from "../assets/logo.png"
 
 const Search = styled('div')(({ theme }) => ({
     position: 'relative',
