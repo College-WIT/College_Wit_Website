7ef9b1ea2c7ea7990b1be3a14073cab39ba770ee
kush-munot <munot.kgm@gmail.com>
2022-06-02 13:52:43 +0530

    Folder str changes

--- rcoem-overflow/src/App.js
+++ rcoem-overflow/src/App.js
@@ -4,7 +4,7 @@ import Footer from './Components/Footer'
 import Walkaround from "./Components/walkaround";
 import Boxes from './Components/HeroSec.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Contributors from "./ContributorsSection";
+import Contributors from "./Components/ContributorsSection";
 
 export default function App() {
   return (
