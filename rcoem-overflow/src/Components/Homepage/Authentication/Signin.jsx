a2bc89091dc6ad9194718cf4b676715e1e8c34d1
Anjali2201 <anjalikushwaha031@gmail.com>
2022-06-15 03:18:51 +0530

    minor changes

--- rcoem-overflow/src/Components/Homepage/Authentication/Signin.jsx
+++ rcoem-overflow/src/Components/Homepage/Authentication/Signin.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Grid, Paper, TextField, Button, Typography, Link } from '@mui/material'
 
-const buttons = { margin: '8px 0', backgroundColor: "#E26639"}
+const buttons = { margin: '8px 0', backgroundColor: "#E26639" }
 const Signin = () => {
 
   const paperStyle = { padding: 40, height: '70vh', width: 400, margin: "0 auto" }
 
   return (
-    
+
     <Grid sx={{
       padding: 10,
       height: 600
@@ -21,14 +21,8 @@ const Signin = () => {
         <Button style={buttons} type='submit' variant='contained' color='primary'>Sign up</Button>
         <Typography >
           <Link href="#" >
-            Forgot password ?
+            Forgot password?
           </Link>
-        </Typography>
-        <Typography > Do you have an account ?
-          <Link style={{ textDecoration: "None" }} to={`/signup`} >
-            Sign Up
-          </Link>
-        
         </Typography>
       </Paper>
     </Grid>
