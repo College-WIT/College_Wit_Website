a629ef4a063802639024bc094f40f7a39e226df1
kush-munot <munot.kgm@gmail.com>
2022-06-15 23:18:51 +0530

    Added Axios GET Request on Questions Page

--- rcoem-overflow/src/Components/Questions/QuestionsPage.jsx
+++ rcoem-overflow/src/Components/Questions/QuestionsPage.jsx
@@ -7,7 +7,8 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack';
-
+import { Component } from 'react'
+import axios from 'axios'
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -19,161 +20,185 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const quickAccBar = ["Home", "Questions", "Answered", "Unanswered", "Trending"];
 const queTags = ["Tags", "Tags", "Tags", "Tags", "Tags"];
-const dummyQue = [
-    {
-        que: "Create a class TimeZone which will add functionality to convert the time from one time zone to another time zone.Note: Class Time zone  uses the TIme Object and uses the Add and Subtract methods of Time Class.",
-        upvotes: 10,
-        ans: 20,
-        views: 300
-    }, {
-        que: "Culpa ex tempor esse Lorem laboris consectetur ullamco.Veniam pariatur anim velit amet Lorem id cillum.",
-        upvotes: 20,
-        ans: 30,
-        views: 330
-    }, {
-        que: "Lorem anim sint adipisicing esse.Commodo laboris occaecat Lorem laboris irure nulla irure duis voluptate aliqua adipisicing magna ullamco deserunt.",
-        upvotes: 40,
-        ans: 50,
-        views: 310
+
+
+class QuestionsPages extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            QuestionsData: [],
+            errorMsg: ""
+        }
     }
-]
 
-export default function QuestionsPage() {
-    return (
-        <Box sx={{ flexGrow: 1, backgroundColor: "#d9d9d9", padding: 2 }}>
+    componentDidMount() {
+        axios.get("https://rcoem-overflow-backend.herokuapp.com/view_all_questions")
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    QuestionsData: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                this.setState({
+                    errorMsg: "Error retrieving data"
+                })
+            })
+    }
 
-
-            <Grid container spacing={2}>
-                <Grid item xl={3} lg={2} md={2} sm={2} xs={12}>
-                    <Item sx={{border: 1}}>Quick Access
-                        {quickAccBar.map((content) => (
-                            <Grid sx={{
-                                padding: 2,
-                                '@media (max-width:780px)': {
-                                    padding: '0 ,0, 0, 0'
-                                }
-                            }}>
-                                <Button sx={{
-                                    fontSize: 15,
-                                    backgroundColor: "#E26639",
-                                    width: 130,
-                                    height: 35,
-                                    borderRadius: 10,
-                                    "&:hover": {
-                                        backgroundColor: "#41D450",
-                                        opacity: 10,
-                                    },
-                                    '@media (max-width:1000px)': {
-                                        fontSize: 12,
-                                        width: 100,
-                                        height: 20,
-                                    },
-                                    '@media (max-width:900px)': {
-                                        fontSize: 10,
-                                        width: 90,
-                                        height: 20,
-                                    },
-                                    '@media (max-width:800px)': {
-                                        fontSize: 8,
-                                        width: 70,
-                                        height: 20,
-                                    }
-                                }}
-                                    variant="contained" disableElevation>
-
-                                    <Link style={{ textDecoration: "None", color: "white" }} to={`/${content}`}>
-                                        {/* {quickAccBar} */}
-                                        {content}
-                                    </Link>
-                                </Button>
-                            </Grid>
-                        ))}
-                    </Item>
+    render() {
+        const { QuestionsData, errorMsg } = this.state
+        return (
+            <Box sx={{ flexGrow: 1, backgroundColor: "#d9d9d9", padding: 2 }}>
+                <Grid container justifyContent="center" sx={{
+                    paddingBottom: 1
+                }}>
+                    <h3>Questions Page</h3>
                 </Grid>
 
-                <Grid item xl={7} lg={8} md={8} sm={8} xs={12}>
-                    <Grid item >
-                        <Item sx={{ border: 1 }}>
-                            {dummyQue.map((content) => (
-                                <Box component="span" >
-                                    <Typography  variant="h6" sx={{ color: "black", textAlign: "left", padding: 2 }}>
-                                        {content.que}
-                                    </Typography>
-                                    <Stack direction="row" spacing={2} >
-                                        <Item elevation={0}>
-                                            <Link style={{ textDecoration: "None", color: "black" }} to={`/answers`}>
-                                                <Button variant="outlined">See Full Answer</Button>
-                                            </Link>
-                                        </Item>
-                                        <Stack direction="row" spacing={2} sx={{
-                                            paddingLeft: 32,
-                                            '@media (max-width:1000px)': {
-                                                paddingLeft: 20
-                                            },
-                                            '@media (max-width:850px)': {
-                                                paddingLeft: 1
-                                            }
-                                        }}>
-                                            <Item elevation={0}>
-                                                <Typography variant="subtitle1" color='black'>{content.views}</Typography>
-                                                <Typography variant="subtitle2" color="black">Views</Typography>
-                                            </Item>
-                                            <Item elevation={0}>
-                                                <Typography variant="subtitle1" color='black'>{content.upvotes}</Typography>
-                                                <Typography variant="subtitle2" color="black">Upvotes</Typography>
-                                            </Item>
-                                            <Item elevation={0}>
-                                                <Typography variant="subtitle1" color='black'>{content.ans}</Typography>
-                                                <Typography variant="subtitle2" color="black">Answers</Typography>
-                                            </Item>
-                                        </Stack>
-                                    </Stack>
-                                </Box>
+                <Grid container spacing={2}>
+                    <Grid item xl={3} lg={2} md={2} sm={2} xs={12}>
+                        <Item>Quick Access
+                            {quickAccBar.map((content) => (
+                                <Grid sx={{
+                                    padding: 2,
+                                    '@media (max-width:780px)': {
+                                        padding: '0 ,0, 0, 0'
+                                    }
+                                }}>
+                                    <Button sx={{
+                                        fontSize: 15,
+                                        backgroundColor: "#E26639",
+                                        width: 130,
+                                        height: 35,
+                                        borderRadius: 10,
+                                        "&:hover": {
+                                            backgroundColor: "#41D450",
+                                            opacity: 10,
+                                        },
+                                        '@media (max-width:1000px)': {
+                                            fontSize: 12,
+                                            width: 100,
+                                            height: 20,
+                                        },
+                                        '@media (max-width:900px)': {
+                                            fontSize: 10,
+                                            width: 90,
+                                            height: 20,
+                                        },
+                                        '@media (max-width:800px)': {
+                                            fontSize: 8,
+                                            width: 70,
+                                            height: 20,
+                                        }
+                                    }}
+                                        variant="contained" disableElevation>
+
+                                        <Link style={{ textDecoration: "None", color: "white" }} to={`/${content}`}>
+                                            {/* {quickAccBar} */}
+                                            {content}
+                                        </Link>
+                                    </Button>
+                                </Grid>
                             ))}
                         </Item>
                     </Grid>
 
+                    <Grid item xl={7} lg={8} md={8} sm={8} xs={12}>
+                        <Grid item >
+                            <Item>
+                                {QuestionsData.map((content) => (
+                                    <Box component="span" sx={{ p: 2 }} >
+                                        <Typography variant="h6" sx={{ color: "black", textAlign: "left", padding: 2 }}>
+                                            {content.question}
+                                        </Typography>
+                                        <Stack direction="row" spacing={2} >
+                                            <Item elevation={0}>
+                                                <Link style={{ textDecoration: "None", color: "black" }} to={`/answers`}>
+                                                    <Button variant="outlined">See Full Answer</Button>
+                                                </Link>
+                                            </Item>
+                                            <Stack direction="row" spacing={2} sx={{
+                                                paddingLeft: 32,
+                                                '@media (max-width:1000px)': {
+                                                    paddingLeft: 20
+                                                },
+                                                '@media (max-width:850px)': {
+                                                    paddingLeft: 1
+                                                },
+                                            }}>
+                                                <Item elevation={0}>
+                                                    <Typography variant="subtitle1" color='black'>{content.views}</Typography>
+                                                    <Typography variant="subtitle2" color="black">Views</Typography>
+                                                </Item>
+                                                <Item elevation={0}>
+                                                    <Typography variant="subtitle1" color='black'>{content.upvotes}</Typography>
+                                                    <Typography variant="subtitle2" color="black">Upvotes</Typography>
+                                                </Item>
+                                                <Item elevation={0}>
+                                                    <Typography variant="subtitle1" color='black'>{content.no_of_answers}</Typography>
+                                                    <Typography variant="subtitle2" color="black">Answers</Typography>
+                                                </Item>
+                                            </Stack>
+                                        </Stack>
 
-                </Grid>
-                <Grid item xl={3} lg={2} md={2} sm={2} xs={12}>
+                                    </Box>
 
-                    <Item sx={{border: 1}}>Tags
-                        {queTags.map((content) => (
-                            <Grid sx={{ padding: 1 }}>
+                                ))}
+
+
+
+
+
+                            </Item>
+                        </Grid>
+
+
+                    </Grid>
+                    <Grid item xl={3} lg={2} md={2} sm={2} xs={12}>
+
+                        <Item>Tags
+                            {queTags.map((content) => (
+                                <Grid sx={{ padding: 1 }}>
+                                    <Button sx={{
+                                        backgroundColor: "#E26639",
+                                        width: 100,
+                                        height: 25,
+                                        borderRadius: 10,
+                                        "&:hover": {
+                                            backgroundColor: "#41D450",
+                                            opacity: 10,
+                                        }
+                                    }}
+                                        variant="contained" disableElevation>
+                                        {content}
+                                    </Button>
+                                </Grid>
+                            ))}
+
+                            <Grid sx={{ paddingTop: 10 }}>
                                 <Button sx={{
-                                    backgroundColor: "#E26639",
-                                    width: 100,
-                                    height: 25,
+                                    backgroundColor: "#20D867",
+                                    width: 150,
+                                    height: 60,
                                     borderRadius: 10,
+                                    fontSize: 15,
                                     "&:hover": {
-                                        backgroundColor: "#41D450",
+                                        backgroundColor: "#E26639",
                                         opacity: 10,
                                     }
-                                }}
-                                    variant="contained" disableElevation>
-                                    {content}
+                                }} variant="contained" disableElevation>
+                                    Post a Question
                                 </Button>
                             </Grid>
-                        ))}
-
-                        <Grid sx={{ paddingTop: 10 }}>
-                            <Button sx={{
-                                backgroundColor: "#20D867",
-                                width: 150,
-                                height: 60,
-                                borderRadius: 10,
-                                fontSize: 15,
-                                "&:hover": {
-                                    backgroundColor: "#E26639",
-                                    opacity: 10,
-                                }
-                            }} variant="contained" disableElevation>
-                                Post a Question
-                            </Button>
-                        </Grid>
-                    </Item>
+                        </Item>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
-    );
+            </Box>
+        );
+    }
 }
+
+export default QuestionsPages
