03d6cd643966ce33cde62a30acbe76b5a4fb96b7
Anjali2201 <anjalikushwaha031@gmail.com>
2022-06-26 00:40:01 +0530

    minor changes

--- rcoem-overflow/src/Components/Homepage/ContributorsSection.jsx
+++ rcoem-overflow/src/Components/Homepage/ContributorsSection.jsx
@@ -7,12 +7,13 @@ import { Avatar } from '@mui/material';
 import { CardHeader } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#E26639',
+    backgroundColor:'#3C3938',
     ...theme.typography.body2,
     margin: theme.spacing(2),
     textAlign: 'center',
     color: '#fff',
-    height: 70,
+    height: 100,
+    width: 200
 }));
 
 export default function Contributors() {
@@ -31,23 +32,55 @@ export default function Contributors() {
             }}>
                 <Grid container spacing={4}>
                     <Grid item xs={5}>
-                        <Item><CardHeader sx={{
+                    <Item><CardHeader sx={{
                             alignItems: 'left',
-                            textAlign: 'left',
-                        }}
+                            textAlign: 'left'}}
                             avatar={
                                 <Avatar
+                                sx={{
+                                    height: 60,
+                                    width:60}}
+                            
                                     alt="Remy Sharp"
                                     src="https://cdn0.iconfinder.com/data/icons/education-and-school-filled-outline-1/128/Primary_school_student_study_school_girl_avatar_child-1024.png"
                                 />
                             }
                             title="Item"
-                        />``
+                        />
                         </Item>
-                        <Item>Item</Item>
-                        <Item>Item</Item>
-                        <Item>Item</Item>
-                        <Item>Item</Item>
+                        <Item><CardHeader sx={{
+                            alignItems: 'left',
+                            textAlign: 'left'}}
+                            avatar={
+                                <Avatar
+                                sx={{
+                                    height: 60,
+                                    width:60}}
+                            
+                                    alt="Remy Sharp"
+                                    src="https://cdn0.iconfinder.com/data/icons/education-and-school-filled-outline-1/128/Primary_school_student_study_school_girl_avatar_child-1024.png"
+                                />
+                            }
+                            title="Item"
+                        /></Item>
+                        <Item><CardHeader sx={{
+                            alignItems: 'left',
+                            textAlign: 'left',
+                            }}
+                            avatar={
+                                <Avatar
+                                    sx={{
+                                        height: 60,
+                                        width:60}}
+                                
+                                    alt="Remy Sharp"
+                                    src="https://cdn0.iconfinder.com/data/icons/education-and-school-filled-outline-1/128/Primary_school_student_study_school_girl_avatar_child-1024.png"
+                                />
+                            }
+                            title="Item"
+                        /></Item>
+                        {/* <Item>Item</Item>
+                        <Item>Item</Item> */}
                     </Grid>
                     <Grid item xs={7}>
                         <Item>Demo description</Item>
