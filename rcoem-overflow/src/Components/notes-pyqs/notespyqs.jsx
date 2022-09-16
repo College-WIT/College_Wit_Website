c3f70f273e60b792c90780b3b93c612fbb6c481c
Kush-munot <munot.kgm@gmail.com>
2022-09-16 17:58:58 +0530

    PYQ Done :))

--- rcoem-overflow/src/Components/notes-pyqs/notespyqs.jsx
+++ rcoem-overflow/src/Components/notes-pyqs/notespyqs.jsx
@@ -5,6 +5,7 @@ import Folder from './card.jsx'
 import { Link } from 'react-router-dom';
 import '../../App'
 
+
 const notespyqs = () => {
     const data = [
         {
@@ -41,37 +42,39 @@ const notespyqs = () => {
         },
     ];
     return (
+        <div>
+            
+            <Grid container sx={{
+                justifyContent: "center"
 
-        <Grid container sx={{
-            justifyContent: "center"
-
-        }}>
-            <Grid xl={12} xs={12} md={12}>
-                <Typography sx={{
-                    fontFamily: "monospace",
-                    fontSize: "30px",
-                    textAlign: "center",
-                    marginTop: "1%",
-                }}>
-                    Semesters
-                </Typography></Grid>
-
-            <Grid container xs={9} md={7} xl={6} sx={{
-                justifyContent: "center",
-                // width: "50%",
-                marginTop: "3%",
-                marginBottom: "5%"
             }}>
-                {data.map((d) => (
-                    <Link style={{ textDecoration: "None", color: "white" }} to={"/" + d.title}>
-                        <Grid item ><Folder sem={d.title} /> </Grid>
-                    </Link>
-                ))}
+                <Grid xl={12} xs={12} md={12}>
+                    <Typography sx={{
+                        fontFamily: "monospace",
+                        fontSize: "30px",
+                        textAlign: "center",
+                        marginTop: "1%",
+                    }}>
+                        Semesters
+                    </Typography></Grid>
 
+                <Grid container xs={9} md={7} xl={6} sx={{
+                    justifyContent: "center",
+                    // width: "50%",
+                    marginTop: "3%",
+                    marginBottom: "5%"
+                }}>
+                    {data.map((d) => (
+                        <Link style={{ textDecoration: "None", color: "white" }} to={"/" + d.title}>
+                            <Grid item ><Folder sem={d.title} /> </Grid>
+                        </Link>
+                    ))}
+
+
+                </Grid>
 
             </Grid>
-
-        </Grid>
+        </div>
     );
 }
 
