e813f4f92b6e38a51b02dcfae619a4079844b482
Anjali2201 <anjalikushwaha031@gmail.com>
2022-10-25 18:09:40 +0530

    Homepage

--- rcoem-overflow/src/Pages/HomePage/Home.jsx
+++ rcoem-overflow/src/Pages/HomePage/Home.jsx
@@ -4,8 +4,6 @@ import Footer from "../../Components/Footer";
 import Walkaround from "../../Components/Homepage/walkaround";
 import Boxes from "../../Components/Homepage/HeroSec.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Contributors from "../../Components/Homepage/ContributorsSection";
-import axios from "axios";
 import { Divider } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Counter from "../../Components/Homepage/counter";
