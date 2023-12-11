1b75cfe13189855cd53f467c10bb68e56ea2092f
Kush Munot <munot.kgm@gmail.com>
2023-12-11 15:35:39 +0530

    ✅ Sixth Sem Done⚡

--- src/Components/notes-pyqs/Sems/Semester 6/DWBI.jsx
+++ src/Components/notes-pyqs/Sems/Semester 6/DWBI.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
@@ -8,45 +8,66 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Navbar from "../../../Navbar";
 import Lottie from "react-lottie";
 import * as animationData from "../../../../Assets/BookLottie.json";
+import axios from 'axios';
+import Loading from "../Loading";
+
+const modifyData = (data) => {
+    return data.map((item, index) => {
+        // Modify URL if it matches the specified format
+        const modifiedURL = item.URL.includes("drive.google.com/file/d/") ?
+            item.URL.replace(/\/view\?usp=drivesdk$/, "/preview") :
+            item.URL;
+
+        return {
+            ...item,
+            id: index + 1,
+            title: "PYQ",
+            panel: "panel" + (index + 1),
+            URL: modifiedURL,
+        };
+    });
+};
+
 
 const DWBI = () => {
-    const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = useState(false);
+    const [allData, setAllData] = useState();
+    const [DWBI_Data, setDWBIData] = useState();
+    const [loading, setLoading] = useState(true);
+
+    const getMyPostData = async () => {
+        try {
+            const res = await axios.get("https://script.google.com/macros/s/AKfycbzwkPSx-o2fZ--e1ZpiXum1FSRVzdMnUTtoCWnsqpgVyITWz6zrn2PyHRupkjBmqkhVpg/exec");
+            setAllData(res.data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await new Promise(resolve => setTimeout(resolve, 4000)); // Simulate loader delay
+            getMyPostData();
+            setLoading(false);
+        };
+
+        fetchData();
+    }, []);
+
+    function filterBySubstring(dataArray, substring) {
+        return dataArray.filter(item => item["Full Path"].includes(substring));
+    }
+
+    useEffect(() => {
+        if (allData) {
+            const modifiedData = modifyData(allData);
+            setDWBIData(filterBySubstring(modifiedData, "DWBI"));
+        }
+    }, [allData]);
 
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
     };
-    const data = [
-        {
-            id: 1,
-            title: "Textbook",
-            panel: "panel1",
-            url: " ",
-        },
-        {
-            id: 2,
-            title: "PYQ 2020",
-            panel: "panel2",
-            url: "https://drive.google.com/file/d/1IZkxxPE4cxnLc5FHtkfyxEyNdwBW6ZWw/preview",
-        },
-        {
-            id: 3,
-            title: "PYQ 2018",
-            panel: "panel3",
-            url: "https://drive.google.com/file/d/1W_TPZ4R_7IE93_jqiBUmvRbGKrWfMmaZ/preview",
-        },
-        {
-            id: 4,
-            title: "PYQ 2019",
-            panel: "panel4",
-            url: "https://drive.google.com/file/d/1F1hv14zLJDVr7l75UXDrspHhb-FI0Hhl/preview",
-        },
-        {
-            id: 5,
-            title: "PYQ 2019",
-            panel: "panel5",
-            url: "https://drive.google.com/file/d/1LrF6sJ1mVBRjiP0iQDfckfo48QT6LUt4/preview",
-        },
-    ];
 
     const defaultOptions = {
         loop: true,
@@ -59,78 +80,61 @@ const DWBI = () => {
 
     return (
         <div>
-            <Navbar />
-            <Grid container>
-                <Grid xs={6} md={2}>
-                    <Lottie options={defaultOptions} height="90%" width="90%" style={{ marginTop: '20px' }} />
-                </Grid>
-                <Grid xs={6} md={8}>
-                    <Typography
-                        variant="h3"
-                        sx={{
-                            textAlign: "center",
-                            marginTop: "50px",
-                        }}
-                    >
-                        Database Warehousing Notes and PYQ
-                    </Typography>
-                </Grid>
-            </Grid>
-
-            {/* <Grid container spacing={2} columns={10} sx={{
-                marginTop: '50px'
-            }}>
-                {data.map((d) => (
-                    <Grid item xs={8} sx={{ marginLeft: '10%' }}>
-                        <a href={d.url}>
-                            <Stack variant='contained' sx={{
-                                height: '50px',
-                                backgroundColor: '#E26639',
-                                borderRadius: '10px',
-                                textTransform: 'none',
-                                fontSize: '20px',
-                                color: '#fff',
-                                fontWeight: 500,
-                            }}>
-                                {d.title}
-                            </Stack>
-                        </a>
-                    </Grid>
-                ))} 
-
-            </Grid>*/}
-            <div style={{ padding: "30px" }}>
-                {data.map((d) => (
-                    <Accordion
-                        sx={{ backgroundColor: "#444444", margin: "5px" }}
-                        expanded={expanded === `${d.panel}`}
-                        onChange={handleChange(`${d.panel}`)}
-                    >
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon color="warning" />}
-                            aria-controls="panel1bh-content"
-                            id="panel1bh-header"
-                        >
+            {loading ? (
+                <Loading />
+            ) : (
+                <div>
+                    <Navbar />
+                    <Grid container>
+                        <Grid xs={6} md={2}>
+                            <Lottie options={defaultOptions} height="90%" width="90%" style={{ marginTop: '20px' }} />                </Grid>
+                        <Grid xs={6} md={8}>
                             <Typography
-                                sx={{ color: "white", fontWeight: 700, fontSize: "18px" }}
+                                variant="h3"
+                                sx={{
+                                    textAlign: "center",
+                                    marginTop: "50px",
+                                }}
                             >
-                                {d.title}
+                                Database Warehouse and Business Intelligence Notes and PYQ
                             </Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <iframe
-                                title="doc"
-                                src={d.url}
-                                width="640"
-                                height="480"
-                                style={{ position: "relative", left: "20%" }}
-                            ></iframe>
-                        </AccordionDetails>
-                    </Accordion>
-                ))}
-            </div>
+                        </Grid>
+                    </Grid>
+                    <div style={{ padding: "30px" }}>
+                        {DWBI_Data?.map((d) => (
+                            <Accordion
+                                sx={{ backgroundColor: "#444444", margin: "5px" }}
+                                expanded={expanded === `${d.panel}`}
+                                onChange={handleChange(`${d.panel}`)}
+                            >
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon color="warning" />}
+                                    aria-controls="panel1bh-content"
+                                    id="panel1bh-header"
+                                >
+                                    <Typography
+                                        sx={{ color: "white", fontWeight: 700, fontSize: "18px" }}
+                                    >
+                                        {d.Name}
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <iframe
+                                        title="doc"
+                                        src={d.URL}
+                                        width="640"
+                                        height="480"
+                                        style={{ position: "relative", left: "20%" }}
+                                    ></iframe>
+                                </AccordionDetails>
+                            </Accordion>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
 
-export default DWBI
+    );
+};
+
+export default DWBI;
