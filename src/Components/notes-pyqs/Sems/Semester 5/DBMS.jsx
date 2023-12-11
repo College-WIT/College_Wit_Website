ea18091883d37c41247d6f6b073f5faa695f7f3d
Kush Munot <munot.kgm@gmail.com>
2023-12-11 12:36:29 +0530

    ⚡✅

--- src/Components/notes-pyqs/Sems/Semester 5/DBMS.jsx
+++ src/Components/notes-pyqs/Sems/Semester 5/DBMS.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
@@ -8,59 +8,66 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Navbar from "../../../Navbar";
 import Lottie from "react-lottie";
 import * as animationData from "../../../../Assets/BookLottie.json";
+import axios from 'axios';
+import Loading from "../Loading";
+
+const modifyData = (data) => {
+  return data.map((item, index) => {
+    // Modify URL if it matches the specified format
+    const modifiedURL = item.URL.includes("drive.google.com/file/d/") ?
+      item.URL.replace(/\/view\?usp=drivesdk$/, "/preview") :
+      item.URL;
+
+    return {
+      ...item,
+      id: index + 1,
+      title: "PYQ",
+      panel: "panel" + (index + 1),
+      URL: modifiedURL,
+    };
+  });
+};
+
 
 const DBMS = () => {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [allData, setAllData] = useState();
+  const [DBMS_Data, setDBMSData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const getMyPostData = async () => {
+    try {
+      const res = await axios.get("https://script.google.com/macros/s/AKfycbzwkPSx-o2fZ--e1ZpiXum1FSRVzdMnUTtoCWnsqpgVyITWz6zrn2PyHRupkjBmqkhVpg/exec");
+      setAllData(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 1500)); // Simulate loader delay
+      getMyPostData();
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  function filterBySubstring(dataArray, substring) {
+    return dataArray.filter(item => item["Full Path"].includes(substring));
+  }
+
+  useEffect(() => {
+    if (allData) {
+      const modifiedData = modifyData(allData);
+      setDBMSData(filterBySubstring(modifiedData, "DBMS"));
+    }
+  }, [allData]);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
-
-
-    const data = [
-        {
-            id: 1,
-            title: "Textbook",
-            panel: "panel1",
-            url: " ",
-        },
-        {
-            id: 2,
-            title: "PYQ Online Set 1",
-            panel: "panel2",
-            url: "https://drive.google.com/file/d/1mqi237ClHOlfY_gX7P773_xikIgJgoG6/preview",
-        },
-        {
-            id: 3,
-            title: "PYQ 2019 Summer",
-            panel: "panel3",
-            url: "https://drive.google.com/file/d/1sLAiVUK1jBGaqlJSGXvmRJqkEXUwmMLZ/preview",
-        },
-        {
-            id: 4,
-            title: "PYQ 2019 Winter",
-            panel: "panel4",
-            url: "https://drive.google.com/file/d/1WQ55rv5UrhyHqD-mgpnxpQ1yspm_CYY4/preview",
-        },
-        {
-            id: 5,
-            title: "PYQ 2018 Summer",
-            panel: "panel5",
-            url: "https://drive.google.com/file/d/1QFOJC9_uH2Szuv-4QLI36Uva0vb76no9/preview",
-        },
-        {
-            id: 6,
-            title: "PYQ 2018 Winter",
-            panel: "panel6",
-            url: "https://drive.google.com/file/d/13w8owV6P0BoN3CqTZf36qABQfV4z6BOH/preview",
-        },
-        {
-            id: 7,
-            title: "PYQ 2017",
-            panel: "panel7",
-            url: "https://drive.google.com/file/d/17DlckKGVMzpEJ55p0UPYAxvFOXep33GK/preview",
-        },
-    ];
 
   const defaultOptions = {
     loop: true,
@@ -74,60 +81,69 @@ const DBMS = () => {
   return (
     <div>
       <Navbar />
-      <Grid container>
-        <Grid xs={6} md={2}>
-          <Lottie
-            options={defaultOptions}
-            height="90%"
-            width="90%"
-            style={{ marginTop: "20px" }}
-          />
-        </Grid>
-        <Grid xs={6} md={8}>
-          <Typography
-            variant="h3"
-            sx={{
-              textAlign: "center",
-              marginTop: "50px",
-            }}
-          >
-            DBMS
-          </Typography>
-        </Grid>
-      </Grid>
-
-      
-      <div style={{ padding: "30px" }}>
-        {data.map((d) => (
-          <Accordion
-            sx={{ backgroundColor: "#444444", margin: "5px" }}
-            expanded={expanded === `${d.panel}`}
-            onChange={handleChange(`${d.panel}`)}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon color="warning" />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Grid container>
+            <Grid xs={6} md={2}>
+              <Lottie
+                options={defaultOptions}
+                height="90%"
+                width="90%"
+                style={{ marginTop: "20px" }}
+              />
+            </Grid>
+            <Grid xs={6} md={8}>
               <Typography
-                sx={{ color: "white", fontWeight: 700, fontSize: "18px" }}
+                variant="h3"
+                sx={{
+                  textAlign: "center",
+                  marginTop: "50px",
+                }}
               >
-                {d.title}
+                Database Management Systems Notes and PYQs
               </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <iframe
-                title="doc"
-                src={d.url}
-                width="640"
-                height="480"
-                style={{ position: "relative", left: "20%" }}
-              ></iframe>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-      </div>
+            </Grid>
+          </Grid>
+
+
+          <div style={{ padding: "30px" }}>
+            {DBMS_Data?.map((d) => (
+              <Accordion
+                sx={{ backgroundColor: "#444444", margin: "5px" }}
+                expanded={expanded === `${d.panel}`}
+                onChange={handleChange(`${d.panel}`)}
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon color="warning" />}
+                  aria-controls="panel1bh-content"
+                  id="panel1bh-header"
+                >
+                  <Typography
+                    sx={{ color: "white", fontWeight: 700, fontSize: "18px" }}
+                  >
+                    {d.Name}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <iframe
+                    title="doc"
+                    src={d.URL}
+                    width="640"
+                    height="480"
+                    style={{ position: "relative", left: "20%" }}
+                  ></iframe>
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </div>
+        </div>
+
+      )}
+
     </div>
+
   );
 };
 
